47c57ad5abc7bc8713d92472e54d2fa892b3d4f4
João Vitor Fernandes <135078102+JoaoVitorSFernandes@users.noreply.github.com>
2024-04-03 15:22:43 -0300

    Add files via upload

---
+++ Processo Seletivo_RepoGitHub/Fibonacci/Program.cs
@@ -0,0 +1,44 @@
+﻿class Program
+{
+    public static int Fibonacci(int n)
+    {
+        int a = 0;
+        int b = 1;
+
+        for (int i = 0; i < n; i++)
+        {
+            int temp = a;
+            a = b;
+            b = temp + b;
+        }
+        return a;
+    }
+
+    static void Main()
+    {
+        int i = 0;
+
+        Console.Write("Informe um número para verificar se está na sequência de Fibonacci: ");
+        if (!int.TryParse(Console.ReadLine(), out int res) || res < 0)
+        {
+            Console.WriteLine("Por favor, insira um número inteiro positivo válido.");
+            return;
+        }
+
+        while (true)
+        {
+            var result = Fibonacci(i++);
+
+            if (result == res)
+            {
+                Console.WriteLine($"O número {res} está presente na sequência de Fibonacci.");
+                break;
+            }
+            else if (result > res)
+            {
+                Console.WriteLine($"O número {res} não está presente na sequência de Fibonacci.");
+                break;
+            }
+        }
+    }
+}
---
+++ Processo Seletivo_RepoGitHub/StringReverse/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Informe a string: ");
+        var res = Console.ReadLine();
+        var stringReversed = ReverseString(res);
+        Console.WriteLine(stringReversed);
+    }
+
+    static StringBuilder ReverseString(string word)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = word.Length; i > 0; i--)
+            sb.Append(word[i - 1]);
+
+        return sb;
+    }
+}
